93776a62e3b7ef0977300f219b58207ec9f6fa8f
Asanyth <60246933+Asanyth@users.noreply.github.com>
2021-08-21 00:10:11 -0400

    Updated Ark Page's grid.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@ module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		fontFamily: {
+			roboto: ['Roboto'],
+			orbitron: ['Orbitron']
+		},
 		extend: {
 			backgroundImage: theme => ({
-				'forest': "url('public/pages/games/ark/images/forest.jpg)",
+				'forest': "url('pages/games/ark/images/forest.jpg')",
 			}),
-			fontFamily: {
-				roboto: ['Roboto'],
-				orbitron: ['Orbitron']
-			},
 		},
 	},
 	variants: {
